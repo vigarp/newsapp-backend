ee3aa22c42cb2be9dd4076d24a08a21579429e66
vigarp <vigar.8421@outlook.com>
2022-03-30 14:43:58 +0700

    add more routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,19 @@ const route = express.Router();
 // import route module from user_routes & usersController
 const usersController = require('../controllers/users_controller');
 const articlesController = require('../controllers/articles_controller');
+const {protect, isAdmin} = require('../middleware/auth')
 // declare router
 
 route.post('/register', usersController.addUser);
 route.post('/login', usersController.loginUser);
 
-route.get('/users', usersController.listUsers);
-route.get('/users/:id', usersController.detailUser);
+route.get('/users', protect, usersController.listUsers);
+route.get('/users/:id', protect, usersController.detailUser);
 
-route.post('/articles', articlesController.addArticle);
-route.get('/articles', articlesController.listArticles);
-route.get('/articles/:id', articlesController.detailArticle);
+route.post('/articles', protect, articlesController.addArticle);
+route.get('/articles', protect, articlesController.listArticles);
+route.get('/articles/:id', protect, articlesController.detailArticle);
+route.delete('/articles/:id', protect, isAdmin, articlesController.deleteArticle);
+route.get('/articles/pending/:id', protect, isAdmin, articlesController.updateArticle);
 
 module.exports = route
