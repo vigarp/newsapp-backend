a2e827ebdda812604479db93a39961c1c534412e
vigarp <vigar.8421@outlook.com>
2022-03-30 14:43:47 +0700

    add listarticle nodel

--- src/models/articles_model.js
+++ src/models/articles_model.js
@@ -16,9 +16,9 @@ const addArticle = (dataArticle) => {
 }
 
 // create model for list articles
-const listArticle = () => {
+const listArticle = (searchQuery) => {
     return new Promise((resolve, reject) => {
-        connection.query('SELECT * FROM articles', (error, result) => {
+        connection.query(`SELECT * FROM articles WHERE status LIKE '%${searchQuery}%'`, (error, result) => {
             if (!error) {
                 resolve(result);
             } else {
@@ -43,9 +43,37 @@ const detailArticle = (idArticle) => {
     })
 }
 
+// create model for delete an article
+const deleteArticle = (idArticle) => {
+    return new Promise((resolve, reject) => {
+        connection.query('DELETE FROM articles WHERE id = ?', (idArticle), (error, result) => {
+            if (!error) {
+                resolve(result);
+            } else {
+                console.log(error);
+                reject(error);
+            }
+        })
+    })
+}
 
+// create model for accept an article
+const updateArticle = (idArticle) => {
+    return new Promise((resolve, reject) => {
+        connection.query(`UPDATE articles SET status = 'published' WHERE id = '${idArticle}'`, (error, result) => {
+            if (!error) {
+                resolve(result);
+            } else {
+                console.log(error);
+                reject(error);
+            }
+        })
+    })
+}
 module.exports = {
     addArticle,
     listArticle,
     detailArticle,
+    deleteArticle,
+    updateArticle
 }
